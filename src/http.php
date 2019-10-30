a92adce9dc3a30ba2de208e67c25ea406395346f
Ashutosh <216.ashutosh@gmail.com>
2019-10-30 23:02:45 +0530

    Removed redundant code

--- src/http.php
+++ src/http.php
@@ -4,18 +4,16 @@ namespace MyOperator\Metrics;
 
 class Http extends Metrics {
 
-    const METRIC_TYPE = 'http';
-
-    public function start($path='/')
+    public function __call($name, $args) 
     {
-        $this->setTag('path', $path);
-        $this->count(self::METRIC_TYPE);
-        $this->start_timer(self::METRIC_TYPE);
-        return $this;
-    }
-
-    public function end($code=200, $data=null)
-    {
-        $this->end_timer(self::METRIC_TYPE);
+        $path = isset($args[0]) && is_string($args[0]) ? $args[0] : null;
+        switch($name) {
+            case 'timing': $this->tag(['path' => $path]); break;
+            case 'count': $this->tag(['path' => $path]); break;
+            case 'gauge': $this->tag(['path' => $path]); break;
+            case 'set': $this->tag(['path' => $path]); break;
+        }
+        $this->getClient()->setPrefix('http');
+        return call_user_func_array([$this->getClient(), $name], $args);
     }
 }
--- src/network.php
+++ src/network.php
@@ -4,18 +4,9 @@ namespace MyOperator\Metrics;
 
 class Network extends Metrics {
 
-    const METRIC_TYPE = 'network';
-
-    public function start($name='curl', $path='/')
+    public function __call($name, $args) 
     {
-        $this->setTag(['path' => $path, 'name' => $name]);
-        $this->count(self::METRIC_TYPE);
-        $this->start_timer(self::METRIC_TYPE);
-        return $this;
-    }
-
-    public function end($code=200, $data=null)
-    {
-        $this->end_timer(self::METRIC_TYPE);
+        $this->getClient()->setPrefix('network');
+        return call_user_func_array([$this->getClient(), $name], $args);
     }
 }
